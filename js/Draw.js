ab7d1e108c7e18e43cb8e231f45a0e74c4a11c71
Felippex <bartosz.filip5@gmail.com>
2021-06-27 14:51:32 +0200

    first trial for fixing resize issue

--- js/Draw.js
+++ js/Draw.js
@@ -3,7 +3,7 @@ class Draw {
     this.firstStartPosition = firstPosition;
     this.secondStartPosition = secondPosition;
     this.thirdStartPosition = thirdPosition;
-    this.velocity = 810;
+    this.velocity;
     let _result = this.drawResult()
     this.getDrawResult = () => _result;
   }
@@ -20,7 +20,9 @@ class Draw {
     let distanceForSecond = 0;
     let distanceForThird = 0;
     document.getElementById('bid').disabled = true;
-    resultsArray = [];
+    document.getElementById('start').disabled = true;
+    let eachSlotWidth = firstSlot.getBoundingClientRect().width;
+    this.velocity = eachSlotWidth * 9;
     timeForFirst = (4 + Math.floor(Math.random() * 9 + 1) / 9) * 1000;
     timeForSecond = timeForFirst + 2000 + (Math.floor(Math.random() * 9 + 1) / 9) * 1000;
     timeForThird = timeForSecond + 2000 + (Math.floor(Math.random() * 9 + 1) / 9) * 1000;
@@ -29,7 +31,7 @@ class Draw {
     distanceForSecond = Math.round(this.velocity * timeForSecond / 1000);
     distanceForThird = Math.round(this.velocity * timeForThird / 1000);
 
-    firstSlot.animate([{
+    let mainAnimation = firstSlot.animate([{
       backgroundPositionY: `${this.firstStartPosition}px`
     }, {
       backgroundPositionY: `${this.firstStartPosition + distanceForFirst}px`
@@ -57,20 +59,47 @@ class Draw {
       easing: "linear"
     })
     // resultsArray includes start positions and timeForThird for setTimeout function which render statistics
-    resultsArray.push(Math.floor((this.firstStartPosition + distanceForFirst) / 90 % 9),
-      Math.floor((this.secondStartPosition + distanceForSecond) / 90 % 9),
-      Math.floor(this.thirdStartPosition + distanceForThird) / 90 % 9, timeForThird)
+    resultsArray.push(Math.round((this.firstStartPosition + distanceForFirst) / eachSlotWidth) % 9,
+      Math.round((this.secondStartPosition + distanceForSecond) / eachSlotWidth) % 9,
+      Math.round((this.thirdStartPosition + distanceForThird) / eachSlotWidth) % 9, timeForThird)
+
+    const that = this;
+    const onResizeFunction = window.addEventListener('resize', function () {
+      mainAnimation.commitStyles();
+      console.log(mainAnimation);
+      distanceForFirst = 0;
+      distanceForSecond = 0;
+      distanceForThird = 0;
+      eachSlotWidth = firstSlot.getBoundingClientRect().width;
+      that.velocity = eachSlotWidth * 9;
+      distanceForFirst = Math.round(that.velocity * timeForFirst / 1000);
+      distanceForSecond = Math.round(that.velocity * timeForSecond / 1000);
+      distanceForThird = Math.round(that.velocity * timeForThird / 1000);
+      firstSlot.style.backgroundPositionY = distanceForFirst;
+      // console.log(distanceForFirst);
+      // secondSlot.style.backgroundPositionY = `${distanceForSecond}px`;
+      // thirdSlot.style.backgroundPositionY = `${distanceForThird}px`;
+      // this.firstStartPosition = distanceForFirst;
+      // this.secondStartPosition = distanceForSecond;
+      // this.thirdStartPosition = distanceForThird;
+    })
+
     this.firstStartPosition += distanceForFirst;
     this.secondStartPosition += distanceForSecond;
     this.thirdStartPosition += distanceForThird;
 
     setTimeout(() => {
       document.getElementById('bid').disabled = false
+      document.getElementById('start').disabled = false
     }, timeForThird)
+
+
+    // console.log(this.firstStartPosition, this.secondStartPosition, this.thirdStartPosition);
+
+
 
     return resultsArray
   }
-
 
   getStartPosition() {
     return [this.firstStartPosition, this.secondStartPosition, this.thirdStartPosition]
